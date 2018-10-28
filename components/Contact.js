bee4cc4211c22d49faffc21a402da11251f79276
Damian Godek <damian.godek1@gmail.com>
2018-10-28 22:36:52 +0100

    add jsx

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,20 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('i', {
-          className: 'fa fa-user-tie',
-          src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      )
+      <div className="contactItem">
+        <i className= "fa fa-user-tie" src = "http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico"></i>
+        <p className= "contactLabel">
+          Imię: {this.props.item.firstName}
+        </p>
+        <p className= "contactLabel">
+          Nazwisko: {this.props.item.lastName}
+        </p>
+        <a className = "contactEmail" href = {"mailto" + this.props.item.email}>
+          {this.props.item.emial}
+        </a>
+      </div>
     )
   },
 });
+
+      
